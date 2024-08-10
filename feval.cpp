f2d8229e2ed3d259043e674fd6a957aac50191ea
abhyuday-01 <22103262@mail.jiit.ac.in>
2024-08-10 10:29:25 +0530

    Add files via upload

---
+++ feval.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const int MAXN = 105;
+double a[MAXN][MAXN];
+
+double determinant(int n) {
+    double det = 1.0;
+    for (int i = 0; i < n; i++) {
+        int pivot = i;
+        for (int j = i + 1; j < n; j++) {
+            if (abs(a[j][i]) > abs(a[pivot][i])) {
+                pivot = j;
+            }
+        }
+        if (pivot != i) {
+            swap(a[i], a[pivot]);
+            det *= -1;
+        }
+        if (a[i][i] == 0) {
+            return 0;
+        }
+        det *= a[i][i];
+        for (int j = i + 1; j < n; j++) {
+            double factor = a[j][i] / a[i][i];
+            for (int k = i + 1; k < n; k++) {
+                a[j][k] -= factor * a[i][k];
+            }
+        }
+    }
+    return det;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    double arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+            a[i][j] =arr[i][j];
+        }
+    }
+
+    double det = determinant(n);
+    cout << "Determinant = " << det << endl;
+    return 0;
+}
